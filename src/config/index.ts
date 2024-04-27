ba9acd9590c1e5b04130fc693592914e89a529d8
akhileshsonone <akkhi.sonone@gmail.com>
2024-04-27 16:50:40 +0530

    add postURL to .env and export it from config/index

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,3 +6,4 @@ const username = process.env.MONGODB_USERNAME;
 
 export const port = process.env.PORT;
 export const MONGODB_CONNECTION_URI = `mongodb+srv://${username}:${password}@cluster0.pbh4iqx.mongodb.net/`;
+export const postURL = `${process.env.POST_REQUEST_URL}`;
